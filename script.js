5cf7c5b8946decabd1efeed4031ea19db177260f
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-02-18 12:46:51 +0530

    10 characters shown in home page and footer colour is changed.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var searchBtn = document.getElementById("icon");
 searchBtn.addEventListener("click", showRes);
 
+var request = new XMLHttpRequest();
+request.open('get','https://gateway.marvel.com/v1/public/characters?ts=1&apikey=857b6f8da8acbb134cd01372d3a37317&hash=8d5b1d4072cc9d93c562de8999fcd160');
+request.send();
+
 function showRes()
+{
+    
+}
+function showRes1()
 {
     var searchBox = document.getElementById("search-superhero");
     var name = searchBox.value;
     console.log(name);
-    var request = new XMLHttpRequest();
     request.onload = function(){
         const responseJSON = JSON.parse(request.response);
         // responseJSON.data.results.length
@@ -32,15 +39,9 @@ function showRes()
             name_div.style.fontWeight = 700;
             document.getElementById("result-container").append(img_div);
             img_div.append(name_div);
-
-            if(responseJSON.data.results[i].name == "3-D Man")
-            {
-                console.log(responseJSON.data.results[i].name);
-                console.log(responseJSON.data.results[i].thumbnail.path+'.'+'jpg');
-            }
         }
         
     };
-    request.open('get','https://gateway.marvel.com/v1/public/characters?ts=1&apikey=857b6f8da8acbb134cd01372d3a37317&hash=8d5b1d4072cc9d93c562de8999fcd160');
-    request.send();
 }
+showRes1();
+
