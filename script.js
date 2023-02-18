b123bcb34ff40e86cb99dc91cbfd8222503441eb
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-02-18 16:48:18 +0530

    favourite functionality added.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var searchBtn = document.getElementById("icon");
 searchBtn.addEventListener("click", showRes);
 
+var favs =[];
+
 var request = new XMLHttpRequest();
 request.open('get','https://gateway.marvel.com/v1/public/characters?ts=1&apikey=857b6f8da8acbb134cd01372d3a37317&hash=8d5b1d4072cc9d93c562de8999fcd160');
 request.send();
@@ -9,15 +11,16 @@ function showRes()
 {
     
 }
-function showRes1()
+function showRes1(num)
 {
+    num=10;
     var searchBox = document.getElementById("search-superhero");
     var name = searchBox.value;
     console.log(name);
     request.onload = function(){
         const responseJSON = JSON.parse(request.response);
         // responseJSON.data.results.length
-        for(var i=0; i<10; i++)
+        for(let i=0; i<num; i++)
         {
             var img_div = document.createElement("div");
             // img_div.style.border = "1px solid white";
@@ -25,6 +28,11 @@ function showRes1()
             icon_div.classList.add("fav-icon");
             icon_div.innerHTML = '❤️';
             img_div.append(icon_div);
+
+            icon_div.addEventListener("click", function(){
+                var id =responseJSON.data.results[i].id;
+                favs.push(id);
+            });
             
             img_div.classList.add("image-box");
             img_div.style.height = 15+"rem";
@@ -49,3 +57,58 @@ function showRes1()
 }
 showRes1();
 
+const myfavPage = document.getElementById("my-favs");
+myfavPage.addEventListener("click", function()
+{
+    var res = JSON.parse(request.response);
+
+    var search_div = document.getElementById("content");
+    search_div.style.display="none";
+
+    var result_container = document.getElementById("result-container");
+    result_container.style.display="none";
+
+    var result_container2 = document.getElementById("result-container2");
+    result_container2.style.display = "flex";
+    result_container2.style.justifyContent = "center";
+    result_container2.style.alignItems = "center";
+
+    for(var i=0; i<res.data.results.length; i++)
+    {
+        for(var j=0; j<favs.length; j++)
+        {
+            if(res.data.results[i].id == favs[j])
+            {
+                var img_div = document.createElement("div");
+                // img_div.style.border = "1px solid white";
+                var icon_div = document.createElement("div");
+                icon_div.classList.add("fav-icon");
+                icon_div.innerHTML = '❤️';
+                img_div.append(icon_div);
+
+                icon_div.addEventListener("click", function(){
+                    var id =res.data.results[i].id;
+                });
+                
+                img_div.classList.add("image-box");
+                img_div.style.height = 15+"rem";
+                img_div.style.width = 16+"rem";
+                img_div.style.marginRight = 1+"rem";
+                var image = document.createElement("img");
+                image.src = res.data.results[i].thumbnail.path+'.'+'jpg';
+                image.style.height = 90+"%";
+                image.style.width = 100+"%";
+                img_div.append(image);
+
+                var name_div = document.createElement("div");
+                name_div.style.width = 100+"%";
+                name_div.style.height = 10+"%";
+                name_div.style.textAlign = "center";
+                name_div.innerHTML = res.data.results[i].name;
+                name_div.style.fontWeight = 700;
+                document.getElementById("result-container2").append(img_div);
+                img_div.append(name_div);
+            }
+        }
+    }
+});
